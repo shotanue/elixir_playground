5511a4946ef974be796dc89fdfa5670b4ff16ad2
shotanue <22065594+shotanue@users.noreply.github.com>
2019-01-10 23:40:45 +0900

    初めてのtaskにコメント追加

--- lib/mix/tasks/hello.ex
+++ lib/mix/tasks/hello.ex
@@ -1,6 +1,9 @@
+# Taskを作る時は namespaceをMix.Tasks.XXXXにする
 defmodule Mix.Tasks.Hello do
+  #  Task用のモジュールを読み込む
   use Mix.Task
   
+  # shortdocで mix helpした時のコメントを書ける
   @shortdoc "my first task"
   def run(_) do
     IO.putsElixirPlayground.helloWorld()
