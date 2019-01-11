714083b291b65b111b10484d654ae37326f2ea66
shotanue <22065594+shotanue@users.noreply.github.com>
2019-01-12 01:11:54 +0900

    [add]httpでhello world

---
+++ lib/my_server.ex
@@ -0,0 +1,15 @@
+defmodule MyServer do
+  use Application
+  require Logger
+
+  def start(_type, _args) do
+    children = [
+      {Plug.Cowboy, scheme: :http, plug: MyServer.HelloWorldPlug, options: [port: 8080]}
+    ]
+#    opts = [strategy: :one_for_one, name: MyServer.Supervisor]
+
+    Logger.info("Starting application...")
+
+    Supervisor.start_link(children, [strategy: :one_for_one])
+  end
+end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule ElixirPlayground.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {MyServer,[]}
     ]
   end
 
@@ -23,6 +24,9 @@ defmodule ElixirPlayground.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:cowboy, "~> 2.6"},
+      {:plug, "~> 1.7"},
+      {:plug_cowboy, "~> 2.0"}
     ]
   end
 end
