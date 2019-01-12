2a15cd9fc5a3b5f98a4356ac781f46b1676effb2
shotanue <22065594+shotanue@users.noreply.github.com>
2019-01-12 14:11:37 +0900

    [add]"mix my_server" のようにアプリケーションを起動したいが動かない(すぐプロセスが死ぬ)

---
+++ lib/mix/tasks/my_sever.ex
@@ -0,0 +1,17 @@
+defmodule Mix.Tasks.MyServer do
+  use Mix.Task
+
+  @shortdoc "Start MyServer from Mix.Task"
+  def run(_) do
+    # これはダメらしい
+    # DynamicSupervisor.start_link([name:  MyServer.Supervisor, strategy: :one_for_one])
+    # res = Task.async(MyServer,:start,[[],[]])
+
+    #　phoenixのphx.serverを参考にしたがこれもダメ。
+    #　起動するが、プロセスがすぐ落ちる　mix run --no-halt 的なことをやりたいが不明
+    Application.put_env(:my_server, :serve_endpoints, true, persistent: true)
+
+    Mix.Tasks.Run.run []
+
+  end
+end
